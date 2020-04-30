d451bbab308eb2c8e49c5720b7be6405ea33bae0
TheREYua <pitsa222@gmail.com>
2020-04-30 23:16:44 +0300

    Update sample.java

--- docker/sample.java
+++ docker/sample.java
@@ -1,10 +1,49 @@
-public class AddTwoNumbers {
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-   public static void main(String[] args) {
-        
-      int num1 = 5, num2 = 15, sum;
-      sum = num1 + num2;
+public class RequestParamExample extends HttpServlet {
 
-      System.out.println("Sum of these numbers: "+sum);
-   }
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Request Parameters Example</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h3>Request Parameters Example</h3>");
+        out.println("Parameters in this request:<br>");
+        if (firstName != null || lastName != null) {
+            out.println("First Name:");
+            out.println(" = " + HTMLFilter.filter(firstName) + "<br>");
+            out.println("Last Name:");
+            out.println(" = " + HTMLFilter.filter(lastName));
+        } else {
+            out.println("No Parameters, Please enter some");
+        }
+        out.println("<P>");
+        out.print("<form action=\"");
+        out.print("RequestParamExample\" ");
+        out.println("method=POST>");
+        out.println("First Name:");
+        out.println("<input type=text size=20 name=firstname>");
+        out.println("<br>");
+        out.println("Last Name:");
+        out.println("<input type=text size=20 name=lastname>");
+        out.println("<br>");
+        out.println("<input type=submit>");
+        out.println("</form>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse res)
+    throws IOException, ServletException
+    {
+        doGet(request, response);
+    }
 }

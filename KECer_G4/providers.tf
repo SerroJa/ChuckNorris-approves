229fcc8cb7ddd2814ac7d739fded3d7ab8417bb9
SerroJa <62060830+SerroJa@users.noreply.github.com>
2020-05-04 01:08:11 +0300

    Some edit

--- KECer_G4/providers.tf
+++ KECer_G4/providers.tf
@@ -1,8 +1,3 @@
-# Configure the GitHub Provider
-#provider "github" {
-#	token        = "${var.github_token}"
-#	organization = "${var.github_organization}"
-#}
 provider "google" {
 	  project = var.project
 	  region  = var.region_type
@@ -18,12 +13,8 @@ provider "kubernetes" {
 }
 
 #provider "kubernetes" {
-
- # host = "https://104.196.242.174"
-
   #client_certificate     = "~/.kube/client-cert.pem" //?
   #client_key             = "~/.kube/client-key.pem"
   #cluster_ca_certificate = "~/.kube/cluster-ca-cert.pem"
-
   #load_config_file = false    # when you wish not to load the local config file
 #}
